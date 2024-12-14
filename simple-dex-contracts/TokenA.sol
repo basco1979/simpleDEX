8a8a337124e61a09f160f6e6693881b1b0d1f147
basco1979 <el_basco@hotmail.com>
2024-12-14 16:25:42 -0300

    adding mint function

--- simple-dex-contracts/TokenA.sol
+++ simple-dex-contracts/TokenA.sol
@@ -4,11 +4,13 @@ pragma solidity ^0.8.22;
 import "./node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
+
 contract TokenA is ERC20, Ownable {
-    constructor() 
-    ERC20("TokenA", "TKA")
-    Ownable(msg.sender)
-    {
-            _mint(msg.sender, 1000 * 10 ** decimals()); 
+    constructor() ERC20("TokenA", "TKA") Ownable(msg.sender) {
+        _mint(msg.sender, 1000 * 10**decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
--- simple-dex-contracts/TokenB.sol
+++ simple-dex-contracts/TokenB.sol
@@ -5,10 +5,11 @@ import "./node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract TokenB is ERC20, Ownable {
-    constructor() 
-    ERC20("TokenB", "TKB") 
-    Ownable(msg.sender)
-    {
-        _mint(msg.sender, 1000 * 10 ** decimals()); 
+    constructor() ERC20("TokenB", "TKB") Ownable(msg.sender) {
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
